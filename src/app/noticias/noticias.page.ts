cb6c0a77d37c5372a564fb1258a1806f80614c2a
Asael <asael2@gmail.com>
2018-11-15 13:04:08 -0500

    Slider Refresher

--- src/app/noticias/noticias.page.ts
+++ src/app/noticias/noticias.page.ts
@@ -6,21 +6,40 @@ import { NoticiasService  } from "../servicios/noticias.service";
   templateUrl: './noticias.page.html',
   styleUrls: ['./noticias.page.scss'],
 })
+
 export class NoticiasPage implements OnInit {
   
   articulos:any[];
-
   constructor(public noticias: NoticiasService) { }
+
+  doRefresh(refresher) {
+    let rrff = refresher;
+    console.log('Begin async operation', rrff);
+    this.noticias.getNews().subscribe((res) => {
+      //this.presentarNoticias(res)
+      rrff.detail.complete();
+    });
+    //this.cargarNoticias();
+    
+    // setTimeout(() => {
+    // }, 5000);
+    console.log('Async operation has ended');
+  }
   
   ngOnInit() {
-    this.noticias.getNews().subscribe((res)=>{ 
+    this.cargarNoticias()
+  }
+
+  cargarNoticias(){
+    this.noticias.getNews().subscribe((res) => {
       this.presentarNoticias(res)
     });
   }
 
-  presentarNoticias(respuesta){
+  presentarNoticias(respuesta) {
     this.articulos = respuesta["articles"];
     console.log(this.articulos);
   }
 
+  
 }
